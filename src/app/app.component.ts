2e1a6a39c70d90e0d507e2d5f13de23c835f6020
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-08-14 11:41:20 +0530

    creating app component and container, nav component

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angularbasice';
+  message="this is the message and render through html file";
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {ContainerComponent} from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component'
+
+@NgModule({
+  declarations: [AppComponent, ContainerComponent, NavComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
